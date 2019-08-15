ed94ac58524c3091505b5666fe76bb76b2ac5676
elepro <eleproxxx@gmail.com>
2019-08-15 10:40:45 +0900

    Support Visual Style
    Signed-off-by: elepro <eleproxxx@gmail.com>

--- Anthony/stdafx.cpp
+++ Anthony/stdafx.cpp
@@ -6,3 +6,8 @@
 
 // TODO: このファイルではなく、STDAFX.H で必要な
 // 追加ヘッダーを参照してください。
+
+//Enable Visual Style
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
