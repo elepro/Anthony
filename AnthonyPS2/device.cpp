a31e0d91b9346adee1782b8409771d47a0df5546
elepro <eleproxxx@gmail.com>
2022-10-18 21:52:11 +0900

    device.cppはAnthonyPS2フォルダのファイルを共有するようにした。

--- AnthonyPS2/device.cpp
+++ AnthonyPS2/device.cpp
@@ -5,272 +5,280 @@
 
 HRESULT
 RetrieveDevicePath(
-    _Out_bytecap_(BufLen) LPTSTR DevicePath,
-    _In_                  ULONG  BufLen,
-    _Out_opt_             PBOOL  FailureDeviceNotFound
-    );
+	_Out_bytecap_(BufLen) LPTSTR DevicePath,
+	_In_                  ULONG  BufLen,
+	_Out_opt_             PBOOL  FailureDeviceNotFound
+);
 
 HRESULT
 OpenDevice(
-    _Out_     PDEVICE_DATA DeviceData,
-    _Out_opt_ PBOOL        FailureDeviceNotFound
-    )
+	_Out_     PDEVICE_DATA DeviceData,
+	_Out_opt_ PBOOL        FailureDeviceNotFound
+)
 /*++
 
 Routine description:
 
-    Open all needed handles to interact with the device.
+	Open all needed handles to interact with the device.
 
-    If the device has multiple USB interfaces, this function grants access to
-    only the first interface.
+	If the device has multiple USB interfaces, this function grants access to
+	only the first interface.
 
-    If multiple devices have the same device interface GUID, there is no
-    guarantee of which one will be returned.
+	If multiple devices have the same device interface GUID, there is no
+	guarantee of which one will be returned.
 
 Arguments:
 
-    DeviceData - Struct filled in by this function. The caller should use the
-        WinusbHandle to interact with the device, and must pass the struct to
-        CloseDevice when finished.
+	DeviceData - Struct filled in by this function. The caller should use the
+		WinusbHandle to interact with the device, and must pass the struct to
+		CloseDevice when finished.
 
-    FailureDeviceNotFound - TRUE when failure is returned due to no devices
-        found with the correct device interface (device not connected, driver
-        not installed, or device is disabled in Device Manager); FALSE
-        otherwise.
+	FailureDeviceNotFound - TRUE when failure is returned due to no devices
+		found with the correct device interface (device not connected, driver
+		not installed, or device is disabled in Device Manager); FALSE
+		otherwise.
 
 Return value:
 
-    HRESULT
+	HRESULT
 
 --*/
 {
-    HRESULT hr = S_OK;
-    BOOL    bResult;
+	HRESULT hr = S_OK;
+	BOOL    bResult;
 
-    DeviceData->HandlesOpen = FALSE;
+	DeviceData->HandlesOpen = FALSE;
 
-    hr = RetrieveDevicePath(DeviceData->DevicePath,
-                            sizeof(DeviceData->DevicePath),
-                            FailureDeviceNotFound);
+	hr = RetrieveDevicePath(DeviceData->DevicePath,
+		sizeof(DeviceData->DevicePath),
+		FailureDeviceNotFound);
 
-    if (FAILED(hr)) {
+	if (FAILED(hr)) {
 
-        return hr;
-    }
+		return hr;
+	}
 
-    DeviceData->DeviceHandle = CreateFile(DeviceData->DevicePath,
-                                          GENERIC_WRITE | GENERIC_READ,
-                                          FILE_SHARE_WRITE | FILE_SHARE_READ,
-                                          NULL,
-                                          OPEN_EXISTING,
-                                          FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
-                                          NULL);
+	DeviceData->DeviceHandle = CreateFile(DeviceData->DevicePath,
+		GENERIC_WRITE | GENERIC_READ,
+		FILE_SHARE_WRITE | FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED,
+		NULL);
 
-    if (INVALID_HANDLE_VALUE == DeviceData->DeviceHandle) {
+	if (INVALID_HANDLE_VALUE == DeviceData->DeviceHandle) {
 
-        hr = HRESULT_FROM_WIN32(GetLastError());
-        return hr;
-    }
+		hr = HRESULT_FROM_WIN32(GetLastError());
+		return hr;
+	}
 
-    bResult = WinUsb_Initialize(DeviceData->DeviceHandle,
-                                &DeviceData->WinusbHandle);
+	bResult = WinUsb_Initialize(DeviceData->DeviceHandle,
+		&DeviceData->WinusbHandle);
 
-    if (FALSE == bResult) {
+	if (FALSE == bResult) {
 
-        hr = HRESULT_FROM_WIN32(GetLastError());
-        CloseHandle(DeviceData->DeviceHandle);
-        return hr;
-    }
+		hr = HRESULT_FROM_WIN32(GetLastError());
+		CloseHandle(DeviceData->DeviceHandle);
+		return hr;
+	}
 
-    DeviceData->HandlesOpen = TRUE;
-    return hr;
+	DeviceData->HandlesOpen = TRUE;
+	return hr;
 }
 
 VOID
 CloseDevice(
-    _Inout_ PDEVICE_DATA DeviceData
-    )
+	_Inout_ PDEVICE_DATA DeviceData
+)
 /*++
 
 Routine description:
 
-    Perform required cleanup when the device is no longer needed.
+	Perform required cleanup when the device is no longer needed.
 
-    If OpenDevice failed, do nothing.
+	If OpenDevice failed, do nothing.
 
 Arguments:
 
-    DeviceData - Struct filled in by OpenDevice
+	DeviceData - Struct filled in by OpenDevice
 
 Return value:
 
-    None
+	None
 
 --*/
 {
-    if (FALSE == DeviceData->HandlesOpen) {
+	if (FALSE == DeviceData->HandlesOpen) {
 
-        //
-        // Called on an uninitialized DeviceData
-        //
-        return;
-    }
+		//
+		// Called on an uninitialized DeviceData
+		//
+		return;
+	}
 
-    WinUsb_Free(DeviceData->WinusbHandle);
-    CloseHandle(DeviceData->DeviceHandle);
-    DeviceData->HandlesOpen = FALSE;
+	WinUsb_Free(DeviceData->WinusbHandle);
+	CloseHandle(DeviceData->DeviceHandle);
+	DeviceData->HandlesOpen = FALSE;
 
-    return;
+	return;
 }
 
 HRESULT
 RetrieveDevicePath(
-    _Out_bytecap_(BufLen) LPTSTR DevicePath,
-    _In_                  ULONG  BufLen,
-    _Out_opt_             PBOOL  FailureDeviceNotFound
-    )
+	_Out_bytecap_(BufLen) LPTSTR DevicePath,
+	_In_                  ULONG  BufLen,
+	_Out_opt_             PBOOL  FailureDeviceNotFound
+)
 /*++
 
 Routine description:
 
-    Retrieve the device path that can be used to open the WinUSB-based device.
+	Retrieve the device path that can be used to open the WinUSB-based device.
 
-    If multiple devices have the same device interface GUID, there is no
-    guarantee of which one will be returned.
+	If multiple devices have the same device interface GUID, there is no
+	guarantee of which one will be returned.
 
 Arguments:
 
-    DevicePath - On successful return, the path of the device (use with CreateFile).
+	DevicePath - On successful return, the path of the device (use with CreateFile).
 
-    BufLen - The size of DevicePath's buffer, in bytes
+	BufLen - The size of DevicePath's buffer, in bytes
 
-    FailureDeviceNotFound - TRUE when failure is returned due to no devices
-        found with the correct device interface (device not connected, driver
-        not installed, or device is disabled in Device Manager); FALSE
-        otherwise.
+	FailureDeviceNotFound - TRUE when failure is returned due to no devices
+		found with the correct device interface (device not connected, driver
+		not installed, or device is disabled in Device Manager); FALSE
+		otherwise.
 
 Return value:
 
-    HRESULT
+	HRESULT
 
 --*/
 {
-    BOOL                             bResult = FALSE;
-    HDEVINFO                         deviceInfo;
-    SP_DEVICE_INTERFACE_DATA         interfaceData;
-    PSP_DEVICE_INTERFACE_DETAIL_DATA detailData = NULL;
-    ULONG                            length;
-    ULONG                            requiredLength=0;
-    HRESULT                          hr;
+	BOOL                             bResult = FALSE;
+	HDEVINFO                         deviceInfo;
+	SP_DEVINFO_DATA                  infoData;
+	SP_DEVICE_INTERFACE_DATA         interfaceData;
+	PSP_DEVICE_INTERFACE_DETAIL_DATA detailData = NULL;
+	ULONG                            length;
+	ULONG                            requiredLength = 0;
+	HRESULT                          hr = HRESULT_FROM_WIN32(S_OK);
+	BOOL							 found = false;
+	UINT							 infoIndex = 0;
 
-    if (NULL != FailureDeviceNotFound) {
+	if (NULL != FailureDeviceNotFound) {
 
-        *FailureDeviceNotFound = FALSE;
-    }
+		*FailureDeviceNotFound = FALSE;
+	}
 
-    //
-    // Enumerate all devices exposing the interface
-    //
-    deviceInfo = SetupDiGetClassDevs(&GUID_DEVINTERFACE_USBApplication1,
-                                     NULL,
-                                     NULL,
-                                     DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+	//
+	// Enumerate all devices exposing the interface
+	//
+	deviceInfo = SetupDiGetClassDevs(&GUID_DEVINTERFACE_USB_DEVICE,
+		NULL,
+		NULL,
+		DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
 
-    if (deviceInfo == INVALID_HANDLE_VALUE) {
+	if (deviceInfo == INVALID_HANDLE_VALUE) {
 
-        hr = HRESULT_FROM_WIN32(GetLastError());
-        return hr;
-    }
+		hr = HRESULT_FROM_WIN32(GetLastError());
+		return hr;
+	}
 
-    interfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
+	interfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
+	infoData.cbSize = sizeof(infoData);
 
-    //
-    // Get the first interface (index 0) in the result set
-    //
-    bResult = SetupDiEnumDeviceInterfaces(deviceInfo,
-                                          NULL,
-                                          &GUID_DEVINTERFACE_USBApplication1,
-                                          0,
-                                          &interfaceData);
+	int memberIndex = 0;
+	while (!found)
+	{
+		//
+		// Get the interface in the result set
+		//
+		bResult = SetupDiEnumDeviceInterfaces(deviceInfo,
+			NULL,
+			&GUID_DEVINTERFACE_USB_DEVICE,
+			memberIndex,
+			&interfaceData);
+		memberIndex++;
 
-    if (FALSE == bResult) {
+		if (FALSE == bResult) {
 
-        //
-        // We would see this error if no devices were found
-        //
-        if (ERROR_NO_MORE_ITEMS == GetLastError() &&
-            NULL != FailureDeviceNotFound) {
+			//
+			// We would see this error if no devices were found
+			//
+			if (ERROR_NO_MORE_ITEMS == GetLastError() &&
+				NULL != FailureDeviceNotFound) {
 
-            *FailureDeviceNotFound = TRUE;
-        }
+				*FailureDeviceNotFound = TRUE;
+			}
+			hr = HRESULT_FROM_WIN32(GetLastError());
+			break;
+		}
 
-        hr = HRESULT_FROM_WIN32(GetLastError());
-        SetupDiDestroyDeviceInfoList(deviceInfo);
-        return hr;
-    }
+		//
+		// Get the size of the path string
+		// We expect to get a failure with insufficient buffer
+		//
+		bResult = SetupDiGetDeviceInterfaceDetail(deviceInfo,
+			&interfaceData,
+			NULL,
+			0,
+			&requiredLength,
+			NULL);
 
-    //
-    // Get the size of the path string
-    // We expect to get a failure with insufficient buffer
-    //
-    bResult = SetupDiGetDeviceInterfaceDetail(deviceInfo,
-                                              &interfaceData,
-                                              NULL,
-                                              0,
-                                              &requiredLength,
-                                              NULL);
+		if (FALSE == bResult && ERROR_INSUFFICIENT_BUFFER != GetLastError()) {
 
-    if (FALSE == bResult && ERROR_INSUFFICIENT_BUFFER != GetLastError()) {
+			hr = HRESULT_FROM_WIN32(GetLastError());
+			continue;
+		}
 
-        hr = HRESULT_FROM_WIN32(GetLastError());
-        SetupDiDestroyDeviceInfoList(deviceInfo);
-        return hr;
-    }
+		//
+		// Allocate temporary space for SetupDi structure
+		//
+		detailData = (PSP_DEVICE_INTERFACE_DETAIL_DATA)LocalAlloc(LMEM_FIXED, requiredLength);
 
-    //
-    // Allocate temporary space for SetupDi structure
-    //
-    detailData = (PSP_DEVICE_INTERFACE_DETAIL_DATA)
-        LocalAlloc(LMEM_FIXED, requiredLength);
+		if (NULL == detailData)
+		{
+			hr = E_OUTOFMEMORY;
+			continue;
+		}
 
-    if (NULL == detailData)
-    {
-        hr = E_OUTOFMEMORY;
-        SetupDiDestroyDeviceInfoList(deviceInfo);
-        return hr;
-    }
+		detailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
+		length = requiredLength;
 
-    detailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
-    length = requiredLength;
+		//
+		// Get the interface's path string
+		//
+		bResult = SetupDiGetDeviceInterfaceDetail(deviceInfo,
+			&interfaceData,
+			detailData,
+			length,
+			&requiredLength,
+			NULL);
 
-    //
-    // Get the interface's path string
-    //
-    bResult = SetupDiGetDeviceInterfaceDetail(deviceInfo,
-                                              &interfaceData,
-                                              detailData,
-                                              length,
-                                              &requiredLength,
-                                              NULL);
+		if (FALSE == bResult)
+		{
+			hr = HRESULT_FROM_WIN32(GetLastError());
+			LocalFree(detailData);
+			continue;
+		}
 
-    if(FALSE == bResult)
-    {
-        hr = HRESULT_FROM_WIN32(GetLastError());
-        LocalFree(detailData);
-        SetupDiDestroyDeviceInfoList(deviceInfo);
-        return hr;
-    }
+		//
+		// Give path to the caller. SetupDiGetDeviceInterfaceDetail ensured
+		// DevicePath is NULL-terminated.
+		//
+		hr = StringCbCopy(DevicePath,
+			BufLen,
+			detailData->DevicePath);
 
-    //
-    // Give path to the caller. SetupDiGetDeviceInterfaceDetail ensured
-    // DevicePath is NULL-terminated.
-    //
-    hr = StringCbCopy(DevicePath,
-                      BufLen,
-                      detailData->DevicePath);
-
-    LocalFree(detailData);
-    SetupDiDestroyDeviceInfoList(deviceInfo);
-
-    return hr;
+		LocalFree(detailData);
+		if (StrStr(DevicePath, _T("vid_054c&pid_02ea")) != NULL)
+		{
+			found = TRUE;
+			break;
+		}
+	}
+	SetupDiDestroyDeviceInfoList(deviceInfo);
+	DevicePath[((static_cast<unsigned long long>(BufLen)) - 1) / sizeof(TCHAR)] = 0;
+	return hr;
 }
--- AnthonyPS2/device.h
+++ AnthonyPS2/device.h
@@ -7,10 +7,13 @@
 // Device Interface GUID.
 // Used by all WinUsb devices that this application talks to.
 // Must match "DeviceInterfaceGUIDs" registry value specified in the INF file.
-// 436366c2-1c9c-408d-8a18-794a3cedcefa
+// a5dcbf10-6530-11d2-901f-00c04fb951ed
 //
-DEFINE_GUID(GUID_DEVINTERFACE_USBApplication1,
-    0x436366c2,0x1c9c,0x408d,0x8a,0x18,0x79,0x4a,0x3c,0xed,0xce,0xfa);
+//旧ドライバで使っていたGUID
+//DEFINE_GUID(GUID_DEVINTERFACE_USBApplication1,
+//    0x436366c2,0x1c9c,0x408d,0x8a,0x18,0x79,0x4a,0x3c,0xed,0xce,0xfa);
+DEFINE_GUID(GUID_DEVINTERFACE_USB_DEVICE,
+    0xa5dcbf10, 0x6530, 0x11d2, 0x90, 0x1f, 0x00, 0xc0, 0x4f, 0xb9, 0x51, 0xed);
 
 typedef struct _DEVICE_DATA {
 
